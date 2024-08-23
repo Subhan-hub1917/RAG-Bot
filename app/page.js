b5e0c9be3c42c11732237a52bdcc553a80ee773c
Subhan Qamar <subiq1917@gmail.com>
2024-08-23 21:37:24 +0500

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,133 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useRef, useState } from "react";
+import { Bars } from "react-loader-spinner";
+import Markdown from "react-markdown";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [query,setQuery]=useState({
+    role:'user',
+    content:''
+  })
+  const messagesRef=useRef(null);
+  const [input,setInput]=useState('')
+  const [loading,setLoading]=useState(false)   
+  const [messages,setMessages]=useState([
+    { role:'system',
+      content:'Hi! How can i assist you?'
+    },
+  ])
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  const scrollToBottom=()=>{
+    messagesRef.current?.scrollIntoView({behavior:'smooth'})
+  }
+  useEffect(()=>{
+    scrollToBottom();
+  },[messages])
+  
+  const handleQuery=async(e)=>{
+    e.preventDefault();
+    if(input==''){
+      return
+    }
+    setLoading(true)
+    setQuery(prev=>({...prev,content:input}))
+    query.content=input
+    setMessages(prev=>[...prev,query])
+    setInput('')
+    try{
+      const res= await fetch("https://rate-my-professor-backend-git-main-subhan-hub1917s-projects.vercel.app/api/query-professors", {
+        method: 'POST',  // Add this line to specify the HTTP method
+        headers: {
+          'Content-Type': 'application/json',  // Set the content type to JSON
+        },
+        body: JSON.stringify({ input }),
+      });
+      if (!res.ok) {
+        setLoading(false)
+        const errorText = await res.text(); 
+        throw new Error(`Network response was not ok: ${errorText}`);
+      }
+      const result=await res.json()
+      const systemResponse={
+        role:'system',
+        content:result.choices[0].message.content
+      }
+      setMessages(prev=>[...prev,systemResponse])
+      setLoading(false)
+    }
+    catch(error){
+      alert(`Erorr Requesting to Model ${error.message}`)
+    }
+  }
+return (
+    <main className="relative top-0 h-screen py-0 overflow-hidden z-50 bg-black text-white" >
+      <section className='backdrop-blur-lg h-screen flex flex-col items-center justify-center'>
+          <div className='text-2xl py-3 md:text-5xl px-5 font-semibold my-1 '>RAG-Bot</div>
+          <div className=" md:border overflow-hidden bg-slate-950 md:border-black my-0 md:my-3 rounded-xl h-full w-screen  md:w-1/2  flex flex-col items-center justify-between">
+              {/* message sections */}
+              <div className="overflow-y-auto overflow-x-hidden rounded-xl py-2 px-2 w-full max-h-full">
+              {messages.map((m, index) => (
+                            <div
+                                key={index}
+                                className={`my-1 flex items-start text-center ${m.role === "user" ? "justify-end" : "justify-start"}`}
+                            >
+                                {m.role === "user" ? (
+                                    <>
+                                        <Markdown
+                                            className={`text-sm p-2 inline-block bg-slate-700 text-end rounded-s-2xl rounded-b-2xl`}
+                                        >
+                                            {m.content}
+                                        </Markdown>
+                                        <i className="bi bi-person  w-8 h-8 mr-1 text-3xl"></i>
+                                        
+                                    </>
+                                ) : m.role === "system" && (
+                                    <>
+                                      
+                                        <i className="bi bi-robot w-8 h-8 mr-1 text-3xl"></i>
+                                        <Markdown
+                                            className={`text-sm p-2 inline-block  bg-slate-700 text-left mt-2 mb-2 rounded-e-2xl rounded-b-2xl`}
+                                        >
+                                            {m.content}
+                                        </Markdown>
+                                    </>
+                                )}
+                            </div>
+                        ))}
+                <div ref={messagesRef} />
+              </div>
+              {/* prompt section */}
+              <div className="w-full py-3 px-3 rounded-t-lg bg-black border border-slate-700 rounded-xl">
+                <form onSubmit={handleQuery} className="w-full flex items-center justify-between space-x-3">
+                  <input
+                    placeholder="Write your query here..."
+                    className="text-black h-10 w-full rounded-lg px-3"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                  />
+                  {
+                    loading
+                    ?
+                    <button onClick={()=>setLoading(false)}>
+                      <Bars
+                        height="40"
+                        width="40"
+                        color="#ffffff"
+                        ariaLabel="bars-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
+                        />
+                      </button>
+                    :
+                      <button type="submit" className="bi bi-send-fill rounded-lg  py-2 px-4  text-lg"></button>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                  }
+    
+                </form>
+              </div>
+          </div>
+      </section>
     </main>
-  );
+)
 }
